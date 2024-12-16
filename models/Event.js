ad3944e33df611c06cd29a22af7089986703355a
jochen <jochen@byte23.net>
2024-12-16 12:30:40 +0100

    created a basic model of an event without references to other tables.

---
+++ models/Event.js
@@ -0,0 +1,36 @@
+import sequelize from '../db/db.js';
+import { DataTypes } from 'sequelize';
+import Op from 'sequelize';
+
+export const Event = sequelize.define('Event', {
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    imageUrl: {
+        type: DataTypes.STRING,
+    },
+    startDate: {
+        type: DataTypes.DATE(DataTypes.DATEONLY),
+        allowNull: false,
+    },
+    endDate: {
+        type: DataTypes.DATE(DataTypes.DATEONLY),
+        allowNull: false,
+    },
+    nights: {
+        type: DataTypes.INTEGER(endDate - startDate),
+    },
+    recurring: {
+        type: DataTypes.BOOLEAN,
+    },
+    recurrence_pattern: {
+        type: DataTypes.STRING,
+    },
+    list_ids: {
+        type: DataTypes.ARRAY, // Array of list_ids in the specific event
+    },
+    owner_id: {
+        type: DataTypes.INTEGER, // Refer to user_id
+    },
+});
